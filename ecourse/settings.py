4ebf7e4e7ffcdfea6f78169bbcbea461e3f43d6d
N S <NS011618>
2023-10-10 09:21:26 +0530

    deploying

--- ecourse/settings.py
+++ ecourse/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-8zylnzyg%ah(1(#^ndnggipig1-o@v^c=pvje=l!izzodyr95b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,23 +75,13 @@ WSGI_APPLICATION = 'ecourse.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-#DATABASES = {
- #      "default": {
- #         "ENGINE": 'django.db.backends.sqlite3',
-#          "NAME": BASE_DIR / 'db.sqlite3',
- #      }
-#}
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'N3UEVVqmAjugJstkLXr1',
-        'HOST': 'containers-us-west-119.railway.app',
-        'PORT': '7769',
-    }
+       "default": {
+         "ENGINE": 'django.db.backends.sqlite3',
+          "NAME": BASE_DIR / 'db.sqlite3',
+       }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -133,7 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')],
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
